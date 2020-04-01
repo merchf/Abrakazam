cbd43c8fb5e732968415aead8a7bf9755fa2aad0
merchf <mercherr@ucm.es>
2020-04-01 15:07:28 +0200

    prueba rutas

--- src/scenes/preloader.js
+++ src/scenes/preloader.js
@@ -7,55 +7,55 @@ export default class Preloader extends Phaser.Scene {
 
 
         //carga de imagenes
-        this.load.image("menu_fondo", '/assets/menu/menu_fondo.png');
-        this.load.image("play_button", '/assets/menu/play_button.png');
-        this.load.image("rules_button", '/assets/menu/rules_button.png');
-        this.load.image("credits_button", '/assets/menu/credits_button.png');
-        this.load.image("logo", '/assets/menu/logo.png');
-        this.load.image("bruja", '/assets/bruja/Brujita.png');
+        this.load.image("menu_fondo", '/Abrakazam/assets/menu/menu_fondo.png');
+        this.load.image("play_button", '/Abrakazam/assets/menu/play_button.png');
+        this.load.image("rules_button", '/Abrakazam/assets/menu/rules_button.png');
+        this.load.image("credits_button", '/Abrakazam/assets/menu/credits_button.png');
+        this.load.image("logo", '/Abrakazam/assets/menu/logo.png');
+        this.load.image("bruja", '/Abrakazam/assets/bruja/Brujita.png');
 
         //carga de sprites
-       this.load.spritesheet('brujaDcha', '/assets/bruja/BrujitaAndandoDer.png', { 
+       this.load.spritesheet('brujaDcha', '/Abrakazam/assets/bruja/BrujitaAndandoDer.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaIzq', '/assets/bruja/BrujitaAndandoIzq.png', { 
+        this.load.spritesheet('brujaIzq', '/Abrakazam/assets/bruja/BrujitaAndandoIzq.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaEsc', '/assets/bruja/BrujitaEspaldasEscalar.png', { 
+        this.load.spritesheet('brujaEsc', '/Abrakazam/assets/bruja/BrujitaEspaldasEscalar.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaFDcha', '/assets/bruja/BrujitaFuego.png', { 
+        this.load.spritesheet('brujaFDcha', '/Abrakazam/assets/bruja/BrujitaFuego.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaFIzq', '/assets/bruja/BrujitaFuegoIzq.png', { 
+        this.load.spritesheet('brujaFIzq', '/Abrakazam/assets/bruja/BrujitaFuegoIzq.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaHDcha', '/assets/bruja/BrujitaHielo.png', { 
+        this.load.spritesheet('brujaHDcha', '/Abrakazam/assets/bruja/BrujitaHielo.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaHIzq', '/assets/bruja/BrujitaHieloIzq.png', { 
+        this.load.spritesheet('brujaHIzq', '/Abrakazam/assets/bruja/BrujitaHieloIzq.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaRDcha', '/assets/bruja/BrujitaRayo.png', { 
+        this.load.spritesheet('brujaRDcha', '/Abrakazam/assets/bruja/BrujitaRayo.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaRIzq', '/assets/bruja/BrujitaRayoIzq.png', { 
+        this.load.spritesheet('brujaRIzq', '/Abrakazam/assets/bruja/BrujitaRayoIzq.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaSaltoDcha', '/assets/bruja/BrujitaSaltoDer.png', { 
+        this.load.spritesheet('brujaSaltoDcha', '/Abrakazam/assets/bruja/BrujitaSaltoDer.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
-        this.load.spritesheet('brujaSaltoIzq', '/assets/bruja/BrujitaSaltoIzq.png', { 
+        this.load.spritesheet('brujaSaltoIzq', '/Abrakazam/assets/bruja/BrujitaSaltoIzq.png', { 
             frameWidth: 100, 
             frameHeight: 100
         });
