09733f0dfd6fb4ead45f3356cf09499fcd81d271
Esther <estpen01@ucm.es>
2020-05-10 19:25:24 +0200

    batallafinal cambios

--- src/scenes/batallaFinal.js
+++ src/scenes/batallaFinal.js
@@ -1,4 +1,4 @@
-export default class MainMenu extends Phaser.Scene {
+export default class BatallaFinal extends Phaser.Scene {
 
     constructor() {
       super({ key: 'batallaFinal' });
@@ -6,14 +6,14 @@ export default class MainMenu extends Phaser.Scene {
     preload() { }
   
     create() {
-      //var opciones = ["fuego", "hielo", "rayo"];
+      //opciones = ["fuego", "hielo", "rayo"];
       let opciones = [0, 1, 2];
       var eleccionMaquina, eleccionUsuario;
       var vidaMaquina= 3;
       var vidaUsuario= 6;
 
       this.add.image(0, 0, "fondobatallafinal").setOrigin(0).setDepth(0);
-
+    
       //botones
       let fuegoButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2-50, "fuegoButton").setDepth(1);
       let hieloButton = this.add.image(this.game.renderer.width / 2, this.game.renderer.height / 2+50, "hieloButton").setDepth(1);
@@ -21,21 +21,20 @@ export default class MainMenu extends Phaser.Scene {
       
       this.add.image(this.game.renderer.width/2-250, this.game.renderer.height / 2 + 80, "brujafinal").setDepth(1);
       this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 +80, "mrLion").setDepth(1);
-
   
       //acciones al clicar
 
       fuegoButton.on("pointerup", () => {
         this.add.image(this.game.renderer.width/2-250, this.game.renderer.height / 2 + 80, "brujaF").setDepth(1);
-        this.usuario(0); 
+        this.start.usuario(0); 
             });
       hieloButton.on("pointerup", () => {
         this.add.image(this.game.renderer.width/2-250, this.game.renderer.height / 2 + 80, "brujaH").setDepth(1);
-        this.usuario(1);
+        this.start.usuario(1);
             });
       rayoButton.on("pointerup", () => {
         this.add.image(this.game.renderer.width/2-250, this.game.renderer.height / 2 + 80, "brujaR").setDepth(1);
-        this.usuario(2);
+        this.start.usuario(2);
       });
   }
 
@@ -46,23 +45,41 @@ export default class MainMenu extends Phaser.Scene {
 
 }
 
+//Función para generar la respuesta aleatoria de la máquina
+function aleatorio(minimo, maximo){
+  var numero = Math.floor(Math.random() * (maximo - minimo +1) + minimo);
+  return numero;
+}
+
+//Función para generar el mensaje correspondiente
+function resultado(vidaMaquina,vidaUsuario){
+  if(vidaMaquina==0){
+      document.getElementById('efecto').innerHTML ='<h1>¡GANASTE!</h1>';
+  }else if(vidaUsuario==0){
+      document.getElementById('efecto').innerHTML ='<h1>¡PERDISTE!</h1>';
+  }else{
+      document.getElementById('efecto').innerHTML ='<h1>Sigue luchando</h1>';
+      console.log(vidaUsuario,vidaMaquina);
+  }
+  document.getElementById('efecto').style.display = "";
+}
+
+//Función para controlar el juego
+// fuego->hielo->rayo
 function usuario(eleccionUsuario){
   eleccionUsuario = parseInt(eleccionUsuario);
   eleccionMaquina = aleatorio(0,2);
       if(eleccionUsuario == 0){//el usuario eligio fuego 
           if(opciones[eleccionMaquina] == 1){//si la maquina eligio hielo
-              //document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio papel y tu piedra.</h3>';
-              this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
-              vidaUsuario=vidaUsuario-1;
+              this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionHielo").setDepth(1);
+              vidaMaquina=vidaMaquina-1;
           }else{
               if(opciones[eleccionMaquina] == 2){//si la máquina eligio rayo
-                //document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio tijera y tu piedra.</h3>';
-                this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
-                vidaMaquina=vidaMaquina-1;
+                this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionRayo").setDepth(1);
+                vidaUsuario=vidaUsuario-1;
               }else{
                   if(opciones[eleccionMaquina] == 0){//si la máquina eligio fuego
-                    // document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron FUEGO </h3>';
-                    this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
+                    this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionFuego").setDepth(1);
                   }
               }
           } 
@@ -70,19 +87,16 @@ function usuario(eleccionUsuario){
       }
 
       if(eleccionUsuario == 1){//el usuario eligio hielo 
-          if(opciones[eleccionMaquina] == 2){
-              //document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio tijera y tu papel.</h3>';
-              this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
-              vidaUsuario=vidaUsuario-1;
+          if(opciones[eleccionMaquina] == 2){//si la maquina elige rayo
+              this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionRayo").setDepth(1);
+              vidaMaquina=vidaMaquina-1;
           }else{
-              if(opciones[eleccionMaquina] == 0){
-                  // document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio piedra y tu papel.</h3>';
-                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
-                  vidaMaquina=vidaMaquina-1;
+              if(opciones[eleccionMaquina] == 0){//si la maquina elige fuego
+                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionFuego").setDepth(1);
+                  vidaUsuario=vidaUsuario-1;
               }else{
-                  if(opciones[eleccionMaquina] == 1){
-                  //document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron HIELO.</h3>'; 
-                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
+                  if(opciones[eleccionMaquina] == 1){//si la máquina elige hielo
+                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionHielo").setDepth(1);
                 }
               }
           }
@@ -90,20 +104,17 @@ function usuario(eleccionUsuario){
       }
 
       if(eleccionUsuario == 2) {//el usuario eligio rayo 
-          if(opciones[eleccionMaquina] == 1){
-              //document.getElementById('efecto').innerHTML ='<h1>¡Ganaste!</h1> <h3>La maquina eligio papel y tu tijera.</h3>';
-              this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
-              vidaMaquina=vidaMaquina-1;
+          if(opciones[eleccionMaquina] == 1){//si la maquina elige hielo
+              this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionHielo").setDepth(1);
+              vidaUsuario=vidaUsuario-1; 
           
           }else{
-              if(opciones[eleccionMaquina] == 0){
-                  //document.getElementById('efecto').innerHTML ='<h1>¡Perdiste!</h1> <h3>La maquina eligio piedra y tu tijera.</h3>';
-                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
-                  vidaUsuario=vidaUsuario-1; 
+              if(opciones[eleccionMaquina] == 0){//si la maquina elige fuego
+                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionFuego").setDepth(1);
+                  vidaMaquina=vidaMaquina-1;
               }else{
                   if(opciones[eleccionMaquina] == 2) {
-                  //document.getElementById('efecto').innerHTML ='<h1>¡Empate!</h1> <h3>Ambos eligieron RAYO.</h3>';
-                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "mrLion").setDepth(1);
+                  this.add.image(this.game.renderer.width / 2+250, this.game.renderer.height / 2 + 80, "MrLionRayo").setDepth(1);
                 }
               }
           }
@@ -114,23 +125,6 @@ function usuario(eleccionUsuario){
     boton.on("pointerup", () => {
       this.scene.start(escena);
     });
-  }
-
-  function aleatorio(minimo, maximo){
-    var numero = Math.floor(Math.random() * (maximo - minimo +1) + minimo);
-    return numero;
-  }
-  
-  function resultado(vidaMaquina,vidaUsuario){
-    if(vidaMaquina==0){
-        document.getElementById('efecto').innerHTML ='<h1>¡GANASTE!</h1>';
-    }else if(vidaUsuario==0){
-        document.getElementById('efecto').innerHTML ='<h1>¡PERDISTE!</h1>';
-    }else{
-        document.getElementById('efecto').innerHTML ='<h1>Sigue luchando</h1>';
-        console.log(vidaUsuario,vidaMaquina);
-    }
-    document.getElementById('efecto').style.display = "";
   }
 
   
